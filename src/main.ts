b5a5b1694b163834fa680431cfc4369062124157
Camillarhi <rhitanene@gmail.com>
2022-11-28 18:29:12 +0100

    delete computation by id

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://computate.netlify.app"],
     credentials: true
 
   });
