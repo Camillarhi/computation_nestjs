3290c44b03c1ed82ecb23e3dd2e7871dbf7d6d96
Camillarhi <rhitanene@gmail.com>
2022-11-28 18:21:22 +0100

    delete computation by id

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { config } from 'dotenv';
 config();
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true
