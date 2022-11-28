3880f3e75cfcbde9c6979ed10cf7cd3d3e9ee778
Camillarhi <rhitanene@gmail.com>
2022-11-28 19:04:29 +0100

    added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { config } from 'dotenv';
-import { AppModule } from './app.module';
 config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
